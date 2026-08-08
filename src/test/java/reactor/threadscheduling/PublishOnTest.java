267eff1c625b53acebe984256583ca89a394de56
suhongkim98 <kshz9815@naver.com>
2026-08-08 14:27:04 +0900

    fromCallable + subscribeOn으로 비동기 처리 예제 추가

--- src/test/java/reactor/threadscheduling/PublishOnTest.java
+++ src/test/java/reactor/threadscheduling/PublishOnTest.java
@@ -9,7 +9,7 @@ import reactor.core.scheduler.Schedulers;
 import java.util.concurrent.CountDownLatch;
 
 /**
- * publishOn을 이용한 신호 처리 쓰레드 스케줄링
+ * publishOn을 이용한 신호 처리 쓰레드 스케줄링, 그 이후 연산을 어느 Scheduler에서 실행할지 변경
  *
  * publishOn() 메서드를 이용하면 next, complete, error신호를 별도 쓰레드로 처리할 수 있다.
  * map(), flatMap() 등의 변환도 publishOn()이 지정한 쓰레드를 이용해서 처리한다.
@@ -17,46 +17,6 @@ import java.util.concurrent.CountDownLatch;
  */
 public class PublishOnTest
 {
-    /**
-     * 최초에 2개를 미리 가져올 때를 제외하면 나머지는 모두 publishOn()으로 전달한 스케줄러의 쓰레드가 처리하는 것을 알 수 있다.
-     */
-    @Test
-    void publishOnTest() throws InterruptedException
-    {
-        CountDownLatch latch = new CountDownLatch(1);
-
-        Flux.range(1, 6)
-            .map(i -> {
-                System.out.printf("%s, map 1: %d to %d\n", Thread.currentThread(), i, i + 10);
-                return i + 10;
-            })
-            .publishOn(Schedulers.boundedElastic(), 2) // 두 번째 인자인 2는 스케줄러가 신호를 처리하기 전에 미리 가져올 (prefetch) 데이터 개수이다.
-            .map(i -> {     // publishOn에서 지정한 PUB 스케줄러가 실행
-                System.out.printf("%s, map 2: %d to %d\n", Thread.currentThread(), i, i + 10);
-                return i + 10;
-            })
-            .subscribe(new BaseSubscriber<Integer>() {
-                @Override
-                protected void hookOnSubscribe(Subscription subscription) {
-                    System.out.println(Thread.currentThread() + " hookOnSubscribe");
-                    requestUnbounded();
-                }
-
-                @Override
-                protected void hookOnNext(Integer value) {
-                    System.out.println(Thread.currentThread() + " hookOnNext: " + value);
-                }
-
-                @Override
-                protected void hookOnComplete() {
-                    System.out.println(Thread.currentThread() + " hookOnComplete");
-                    latch.countDown();
-                }
-            });
-
-        latch.await();
-    }
-
     /**
      * publishOn()에 지정한 스케줄러는 다음 publishOn()을 설정할 때까지 적용된다.
      * 첫 번째 publishOn()과 두 번째 publishOn() 사이의 map() 처리는 첫번째 스케줄러가 실행하고
--- src/test/java/reactor/threadscheduling/SubscribeOnTest.java
+++ src/test/java/reactor/threadscheduling/SubscribeOnTest.java
@@ -4,52 +4,82 @@ import org.junit.jupiter.api.Test;
 import org.reactivestreams.Subscription;
 import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import java.util.concurrent.CountDownLatch;
 
 /**
- * subscribeOn을 이용한 구독 처리 쓰레드 스케줄링
+ * subscribeOn을 이용한 구독 처리 쓰레드 스케줄링, 작업 자체가 어느 Scheduler에서 시작될지 결정한다.
  * subscribeOn()을 사용하면 Subscriber가 시퀀스에 대한 request신호를 별도 스케줄러로 처리한다.(시퀀스를 실행할 스케줄러를 지정한다)
  */
-public class SubscribeOnTest
-{
+public class SubscribeOnTest {
     /**
      * subscribeOn()으로 지정한 스케줄러는 시퀀스의 request 요청 처리뿐만 아니라 첫 번째 publishOn() 지정 이전까지의 신호 처리를 실행한다.
      * 따라서 위 코드를 실행하면 Flux.range()가 생성한 시퀀스의 신호 발생뿐만 아니라 map() 실행, Subscriber의 next, complete 신호 처리를 "SUB" 스케줄러가 실행한다.
      * 참고로 시퀀스의 request 요청과 관련된 로그를 보기 위해 log() 메서드를 사용했다.
      */
     @Test
-    void subscribeOnTest() throws InterruptedException
-    {
+    void subscribeOnTest() throws InterruptedException {
         CountDownLatch latch = new CountDownLatch(1);
 
         Flux.range(1, 6)
-            .log() // 보다 상세한 로그 출력 위함
-            .subscribeOn(Schedulers.boundedElastic())
-            .map(i -> {
-                System.out.printf("%s, map 1: %d to %d\n", Thread.currentThread(), i, i + 10);
-                return i + 10;
-            })
-            .subscribe(new BaseSubscriber<Integer>() {
-                @Override
-                protected void hookOnSubscribe(Subscription subscription) {
-                    System.out.println(Thread.currentThread() + " hookOnSubscribe"); // main thread
-                    request(1);
-                }
+                .log() // 보다 상세한 로그 출력 위함
+                .subscribeOn(Schedulers.boundedElastic())
+                .map(i -> {
+                    System.out.printf("%s, map 1: %d to %d\n", Thread.currentThread(), i, i + 10);
+                    return i + 10;
+                })
+                .subscribe(new BaseSubscriber<Integer>() {
+                    @Override
+                    protected void hookOnSubscribe(Subscription subscription) {
+                        System.out.println(Thread.currentThread() + " hookOnSubscribe"); // main thread
+                        request(1);
+                    }
 
-                @Override
-                protected void hookOnNext(Integer value) {
-                    System.out.println(Thread.currentThread() + " hookOnNext: " + value); // SUB 쓰레드
-                    request(1);
-                }
+                    @Override
+                    protected void hookOnNext(Integer value) {
+                        System.out.println(Thread.currentThread() + " hookOnNext: " + value); // SUB 쓰레드
+                        request(1);
+                    }
 
-                @Override
-                protected void hookOnComplete() {
-                    System.out.println(Thread.currentThread() + " hookOnComplete"); // SUB 쓰레드
-                    latch.countDown();
-                }
-            });
+                    @Override
+                    protected void hookOnComplete() {
+                        System.out.println(Thread.currentThread() + " hookOnComplete"); // SUB 쓰레드
+                        latch.countDown();
+                    }
+                });
+
+        latch.await();
+    }
+
+    /**
+     * 동기식 코드를 subscribeOn 활용해 비동기적으로 처리하는 예제
+     * Mono.fromCallable() + subscribeOn()을 사용해 블로킹 동기 코드를 별도 스레드에서 실행한다.
+     * subscribeOn 영역을 제거하면 블로킹 코드가 main 스레드에서 실행되는거 확인 가능
+     */
+    @Test
+    void asyncTest() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(3);
+
+        Mono.fromCallable(() -> {
+                    System.out.println("작업 시작: " + Thread.currentThread().getName());
+
+                    try {
+                        Thread.sleep(2_000);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        throw new RuntimeException(e);
+                    }
+
+                    System.out.println("작업 종료: " + Thread.currentThread().getName());
+                    return "success";
+                }).subscribeOn(Schedulers.boundedElastic())
+                .subscribe(result ->
+                        System.out.println("결과: " + result + ", thread=" + Thread.currentThread().getName())
+                );
+
+        System.out.println("subscribe 이후, thread=" + Thread.currentThread().getName());
 
         latch.await();
     }
